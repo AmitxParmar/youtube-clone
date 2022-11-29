bde57c3cef824ce5654175bd25cbf8bd19f9c306
AmitxParmar <amitparmar901@gmail.com>
2022-11-29 20:37:34 +0530

    Added searchbar but there seem to be issues with the search style, which I'll try to fix later

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import { Box } from '@mui/material';
-import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import { Feed, VideoDetail, ChannelDetail, SearchFeed, NavBar } from "./components";
 
 const App = () => {
-  return ( <>
+  return ( 
   <BrowserRouter>
     <Box sx={{ backgroundColor: '#000', color: 'red' }}>
       <NavBar />
       <Routes>
-        <Route path="/" exact element={<Feed />} />
+        <Route exact path="/" element={<Feed />} />
         <Route path='/video/:id' element={<VideoDetail />} />
         <Route path='/channel/:id' element={<ChannelDetail />} />
         <Route path='/search/:searchTerm' element={<SearchFeed />} />
       </Routes>
     </Box>
   </BrowserRouter>
-  </>)
+  )
 }
 
 export default App;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,17 @@
 import { Stack } from '@mui/material'
 import { Link } from 'react-router-dom'
 
-const NavBar = () => {
-  return (
-    <Stack>
-      <Link />
-    </Stack>
-  )
-}
+import { SearchBar } from './'
+import { logo } from "../utils/constants"
+
+const NavBar = () => (
+  <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
+  <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+    <img src={logo} alt="logo" height={45} />
+  </Link>
+  <SearchBar />
+  </Stack>
+  );
+
 
 export default NavBar
