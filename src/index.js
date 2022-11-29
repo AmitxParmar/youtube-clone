962b9d27f2a1ff260bfeb3ead58925aa8e4dd386
AmitxParmar <amitparmar901@gmail.com>
2022-11-29 21:20:43 +0530

    fixed ui

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
+
 import App from './App';
+import "./index.css"
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
