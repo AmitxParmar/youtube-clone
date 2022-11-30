62f00fbefed8270dacf62d56b1f1ee401092948a
Amit Parmar <amitparmar901@gmail.com>
2022-11-30 17:01:28 +0530

    Update Feed.jsx

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,26 +2,24 @@ import React, { useEffect, useState } from "react";
 import { Box, Stack, Typography } from "@mui/material";
 
 import { fetchFromAPI } from "../utils/fetchFromAPI";
-import { Videos, SideBar } from "./";
+import { Videos, Sidebar } from "./";
 
 const Feed = () => {
   const [selectedCategory, setSelectedCategory] = useState("New");
   const [videos, setVideos] = useState(null);
-  const searchKeyVal = 'search?part=snippet&';
-  
 
   useEffect(() => {
     setVideos(null);
 
-    fetchFromAPI(`${searchKeyVal}${selectedCategory}`)
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
       .then((data) => setVideos(data.items))
-  }, [selectedCategory]);
+    }, [selectedCategory]);
 
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
-        <SideBar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
-
+        <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+        
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
           Copyright © 2022 JSM Media
         </Typography>
