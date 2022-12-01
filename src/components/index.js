334072a175b61793f89f92051134a37e5ca7aba4
Amit Parmar <amitparmar901@gmail.com>
2022-12-01 22:02:12 +0530

    Update index.js
    minor capitalization mistake

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,7 @@ export { default as VideoCard } from './VideoCard';
 export { default as ChannelDetail } from './ChannelDetail';
 export { default as Feed } from './Feed';
 export { default as Loader } from './Loader';
-export { default as Navbar } from './Navbar';
+export { default as NavBar } from './NavBar';
 export { default as SearchBar } from './SearchBar';
 export { default as VideoDetail } from './VideoDetail';
 export { default as SearchFeed } from './SearchFeed';
