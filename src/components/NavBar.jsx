8a83f46312a4c2ca4851d848062fbee41cf2d148
AmitxParmar <amitparmar901@gmail.com>
2022-12-02 23:29:26 +0530

    Added Build Version.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import { Stack } from "@mui/material";
 import { Link } from "react-router-dom";
 
 import { logo } from "../utils/constants";
-import { SearchBar } from "./";
+import { SearchBar } from ".";
 
 const Navbar = () => (
   <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
